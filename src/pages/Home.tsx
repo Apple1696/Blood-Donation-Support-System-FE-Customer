df696ac8326f0674403167c3838a045e6f7bf45e
Apple1696 <ngochingokien@gmail.com>
2025-05-26 22:11:15 +0700

    add capaign carousel to HomePage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,12 @@
 import { Hero151 } from '@/components/hero151'
 import { Hero45 } from '@/components/hero45'
-
+import { Gallery6 } from '@/components/gallery6'
 export default function Home() {
   return (
     <>
       <Hero151/>
       <Hero45/>
+      <Gallery6/>
     </>
   )
 }
